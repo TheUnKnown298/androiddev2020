454e6c8cb7df6241c1485a81722ff4ddb91b9d5c
TheUnKnown298 <47735851+TheUnKnown298@users.noreply.github.com>
2019-11-24 23:56:22 +0700

    Lab 7 Commit
    Eventhough I have tried so many different way to make the app run, but some kind of library confliction alway comes up. My code has no bug or error at all, I really don't know how to fix this thing. Please help me check it out. I was stuck here for too long because of it. Thank you so much!

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForeCastAndWeatherFragment.java
@@ -0,0 +1,30 @@
+package vn.edu.usth.weather;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class ForeCastAndWeatherFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fore_cast_and_weather, container, false);
+    }
+
+}
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -2,6 +2,12 @@ package vn.edu.usth.weather;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.util.Log;
 import android.os.Bundle;
 import android.widget.LinearLayout;
@@ -40,8 +46,36 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("onCreate","Weather activity is being created");
 
-
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.container);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
     }
 
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] { "Hanoi", "Paris", "Toulouse" };
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+        @Override
+        public Fragment getItem(int page) {
+            switch (page) {
+                case 0: return new ForeCastAndWeatherFragment();
+                case 1:return new ForeCastAndWeatherFragment();
+                case 2: return new ForeCastAndWeatherFragment();
+            }
+            return new ForeCastAndWeatherFragment();
+        }
+        @Override
+        public CharSequence getPageTitle(int page) {
 
+            return titles[page];
+        }
+    }
     }
