33bd69ac794d624b5f3ad6a443006e8dcd463522
TheUnKnown298 <47735851+TheUnKnown298@users.noreply.github.com>
2019-12-02 16:12:08 +0700

    Lab 11 Commit

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,18 +1,26 @@
 package vn.edu.usth.weather;
 
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
-import android.util.Log;
-import android.os.Bundle;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class WeatherActivity extends AppCompatActivity {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +30,47 @@ public class WeatherActivity extends AppCompatActivity {
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
+
+
+        InputStream is = getResources().openRawResource(R.raw.kiss);
+
+        writeExternal();
+
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.kiss);
+
+        mp.start();
+
+
+
+
+
 }
+    private void writeExternal() {
+        String filename = "humble.mp3";
+        String filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data";
+
+        try {
+            InputStream is = getApplicationContext().getResources().openRawResource(R.raw.kiss);
+            File myFile = new File(filepath + filename);
+            myFile.createNewFile();
+            OutputStream fout = new FileOutputStream(myFile);
+            fout.write(is.read());
+            fout.close();
+            is.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+
+
+
 
     public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
         private final int PAGE_COUNT = 3;
